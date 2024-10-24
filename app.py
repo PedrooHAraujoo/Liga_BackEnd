d03e237e70945a7e0380c7e8877f0620d50f88dc
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-10-24 17:06:23 -0300

    Criação do main e inclusão da biblioteca Flask e sua devida rota.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, request, render_template, redirect, url_for
-import sqlite3
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from user import adicionar_usuario
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/cadastrar', methods = ['POST'])
+def cadastrar():
+    data = request.json
+    nome = data.get('nome')
+    email = data.get('email')
+    senha = data.get('senha')
+    cargo = data.get('cargo')
+    equipe = data.get('equipe')
+    instagram = data.get('instagram')
+
+    if not nome or not email or not senha or not cargo or not equipe or not instagram:
+        return jsonify({'error': 'Preencha todos os campos!'}), 400
+    adicionar_usuario(nome, email, senha, cargo, equipe, instagram)
+    return jsonify({'message': f'Usuário {nome} foi adicionado com sucesso"'})
+if __name__ == '__main__':
+    app.run(debug=True)
