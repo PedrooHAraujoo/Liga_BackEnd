a136990a944626bb6319ad6cd8d3ad8faf3ac77c
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-10-29 17:53:48 -0300

    Correção da variável do URl do database

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Configurações do banco de dados a partir do .env
-app.config['SQLALCHEMY_DATABASE_URL'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializa o SQLAlchemy com o app
