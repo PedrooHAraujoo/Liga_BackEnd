4951a12d168c3e7f7308da0a3f13436ec4276f3f
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-10-29 16:26:25 -0300

    Atualiza models com os relacionamentos e o (USER.PY) com a integração com o SQLAlchemy

--- user.py
+++ user.py
@@ -1,54 +1,49 @@
-import sqlite3
 import bcrypt
-from database import conectar_banco
+from models import db, Usuario
 
 def adicionar_usuario(nome, email, senha, cargo, equipe, instagram):
     senha_hashed = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
 
     try:
-        conn = conectar_banco()
-        cursor = conn.cursor()
-
-        # Verifica se o email já existe no banco de dados
-        cursor.execute('SELECT * FROM usuarios WHERE email = ?', (email,))
-        usuario_existente = cursor.fetchone()
-
+       # Verifica se o email já existe
+        usuario_existente = Usuario.query.filter_by(email=email).first()
         if usuario_existente:
             return {'error': 'O email já foi registrado.', 'status': 'fail'}, 400
+       # Se o email não existe cria um novo usuário
+        novo_usuario = Usuario(
+            nome=nome,
+            email=email,
+            senha=senha_hashed,
+            cargo=cargo,
+            equipe=equipe,
+            instagram=instagram
+        )
         
-        # Se o email não existe, insere o novo usuário
-        cursor.execute('''
-            INSERT INTO usuarios (nome, email, senha, cargo, equipe, instagram)
-            VALUES (?, ?, ?, ?, ?, ?);  
-        ''', (nome, email, senha_hashed, cargo, equipe, instagram))
-        
-        conn.commit()  # Confirma a transação
+        db.session.add(novo_usuario) # Adiciona à sessão
+        db.session.commit() # Confirma a transação
         return {'message': f'Usuário {nome} foi adicionado com sucesso!', 'status': 'success'}, 201
     
-    except sqlite3.Error as e:
+    except Exception as e:
+        db.session.rollback() # Reverte a transação se houver erro
         return {'error': f'Ocorreu um erro no banco de dados: {str(e)}', 'status': 'fail'}, 500
     
-    finally:
-        conn.close()  # Fecha a conexão com o banco de dados
 def redefinir_senha(email, nova_senha):
     senha_hashed = bcrypt.hashpw(nova_senha.encode('UTF-8'), bcrypt.gensalt())
 
     try:
-        conn = conectar_banco()
-        cursor = conn.cursor()
-
-        cursor.execute('SELECT * FROM usuarios WHERE email = ?', (email,))
-        usuario_existente = cursor.fetchone()
-
+        usuario_existente = Usuario.query.filter_by(email=email).first()
         if not usuario_existente:
             return{'error': 'Usuário não encontrado', "status" : 'fail'}, 404
-        cursor.execute('''
-                UPDATE usuarios SET senha = ? WHERE email = ?
-''',(senha_hashed, email))
         
-        conn.commit()
+        # Atualiza a senha do usuário
+        usuario_existente.senha = senha_hashed
+        db.session.commit()
+
         return{'message' : 'Senha redefinida com sucesso!', 'status': 'sucess'},200
-    except sqlite3.Error as e:
-        return{'error' : f'Ocorreu um erro no banco de dados: {str(e)}', 'status': 'fail'}, 500
-    finally:
-        conn.close()    
+    
+    except Exception as e:
+
+        db.session.rollback() # Reverte a transação 
+
+        return{'error' : f'Ocorreu um erro no banco de dados: {str(e)}', 'status': 'fail'}, 500   
+    
