d4cfef794751fe2262048d63a3dc1099d098c557
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-11-02 18:10:24 -0300

    Reestruturação do banco de dados com a adição da tabela de cargos

--- models.py
+++ models.py
@@ -11,12 +11,15 @@ class Equipe(db.Model):  # db.Model para o ORM mapear a classe à tabela
        # Relacionamento com Ranking e Pontuacao
     rankings = db.relationship('Ranking', backref='equipe', lazy=True)
     pontuacoes = db.relationship('Pontuacao', backref='equipe', lazy=True)
+    usuarios = db.relationship('Usuario', backref='equipe', lazy=True)
+
 class Ranking(db.Model):
     __tablename__ = 'rankings'
     id = db.Column(db.Integer, primary_key=True)
     equipe_id = db.Column(db.Integer, db.ForeignKey('equipes.id'), nullable=False)
     tipo_ranking = db.Column(db.String, nullable=False) 
     pontuacao_total = db.Column(db.Integer, nullable=False)
+
 class Pontuacao(db.Model):
     __tablename__ = 'pontuacoes'
     id = db.Column(db.Integer, primary_key=True)
@@ -24,12 +27,28 @@ class Pontuacao(db.Model):
     valor = db.Column(db.Integer, nullable=False)
     equipe_id = db.Column(db.Integer, db.ForeignKey('equipes.id'), nullable=False)
     data = db.Column(db.Date, nullable=False)
+
+class Cargo(db.Model):
+    __tablename__ = 'cargos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), unique=True, nullable=False)
+    descricao = db.Column(db.String(255))
+    
+    # Relacionamento com um Usuário
+    usuarios = db.relationship('Usuario', backref='cargo', lazy=True)
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     senha = db.Column(db.String, nullable=False)
-    cargo = db.Column(db.String, nullable=False)
-    equipe = db.Column(db.String, nullable=False)
+
+    # Relacionamento com Cargo e Equipe
+    cargo_id = db.Column(db.Integer, db.ForeignKey('cargos.id'), nullable=False)
+    equipe_id  = db.Column(db.Integer, db.ForeignKey('equipes.id'),nullable=False)
+
     instagram = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Usuario {self.nome}>'
