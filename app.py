1eb3804270c131c453d462dc1e162ed169fa1ec6
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-11-13 14:15:59 -0300

    Criação da função create_app, para centralizar a criação e configuração do app dentro de uma função

--- app.py
+++ app.py
@@ -2,31 +2,35 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 from models import db
-from routes import app_routes # Importando o Blueprint
+from routes import app_routes  # Importando o Blueprint
 
-# Carrega as variáveis de ambiente no arquivo .env
-load_dotenv()
+# Função para criar e configurar o app
+def create_app():
+    # Carrega as variáveis de ambiente do arquivo .env
+    load_dotenv()
 
-# Inicializa o app Flask
-app = Flask(__name__)
+    # Inicializa o app Flask
+    app = Flask(__name__)
 
-# Configurações de banco de dados e chave do JWT
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # O SQLALCHEMY_DATABASE_URI estava com URL no final e não estava subindo o servidor, fiz esse pequeno ajuste 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER') # Estava apresentando erro de sintaxe pois o estava passando () na lista. 
+    # Configurações de banco de dados e chave do JWT
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Ajuste da URL do banco
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
 
+    # Inicializa o SQLAlchemy com o app
+    db.init_app(app)
 
-# Inicializa o SQLALCHEMY com o app
-db.init_app(app)
+    # Registra o Blueprint das rotas
+    app.register_blueprint(app_routes, url_prefix='/api')
 
-# Registra o Blueprint das rotas
-app.register_blueprint(app_routes, url_prefix='/api')
+    # Cria as tabelas no banco de dados
+    with app.app_context():
+        db.create_all()
 
-# Cria as tabelas no banco de dados 
-with app.app_context():
-    db.create_all()
+    return app
 
-# Executa o servidor Flask
+# Se o arquivo for executado diretamente, o servidor Flask será iniciado
 if __name__ == '__main__':
+    app = create_app()  # Cria a aplicação Flask
     app.run(debug=True)
